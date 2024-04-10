68c8e842d6a74a924d98d13e7f5941338806c78f
vankovap <petravankova.pv@gmail.com>
2024-04-10 17:27:54 +0200

    Update Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -3,5 +3,6 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+app.Urls.Add("http://0.0.0.0:5000");
 app.MapGet("/", () => Results.Ok("Hello World!"));
 app.Run();
